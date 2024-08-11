fdc451c489d670d21404bf3102be654b0c6afb33
sara-hoxha <sarahoxha.work@gmail.com>
2024-08-12 00:04:42 +0200

    Add code to send JSON

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+// doGet
 function testGS(){
-    const url = "https://script.google.com/macros/s/AKfycbz4taw8Mnam6zFCVwLA_kR1HGccvRv6zggfDZpRtTpmEH6evX4IwEvMbPUBVce-PBhTtg/exec"
+    const url = "https://script.google.com/macros/s/AKfycbyLulAPhfHyD3QJqX0hRYcTG4zY5BIzFVFXTzux2MO5vYfNYn7WgIrFhpA4B8t0Z-RcNA/exec"
 
     fetch(url)
         .then(d => d.json())
@@ -11,3 +12,27 @@ function testGS(){
 
 
 document.getElementById("btn").addEventListener("click", testGS);
+
+// doPost
+function addGS(){
+    const url = "https://script.google.com/macros/s/AKfycbyLulAPhfHyD3QJqX0hRYcTG4zY5BIzFVFXTzux2MO5vYfNYn7WgIrFhpA4B8t0Z-RcNA/exec"
+
+    fetch(url, {
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        credentials: 'omit',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        body: JSON.stringify({name: "John"})
+    });
+
+
+}
+
+
+document.getElementById("btn2").addEventListener("click", addGS);
+
+document.getElementById("btn").addEventListener("click", addGS);
